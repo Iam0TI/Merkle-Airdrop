665628aaa838ce68b20fe836191736ae95e741c0
JustFavour <odeyemifavour1@gmail.com>
2024-09-02 14:05:52 +0100

    readme update

--- script/createMerkleProof.js
+++ script/createMerkleProof.js
@@ -7,7 +7,7 @@ const tree = StandardMerkleTree.load(
 
 // (2)
 for (const [i, v] of tree.entries()) {
-  // generating the proof for address "0x70997970C51812dc3A010C7d01b50e0d17dc79C8" so you can change it
+  // generating the proof for address "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC" so you can change it
   if (v[0] === "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC") {
     // (3)
     const proof = tree.getProof(i);
