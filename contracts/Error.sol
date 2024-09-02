264870e74ba394628095b2d87b470300bbaac29a
JustFavour <80600490+Iam0ti@users.noreply.github.com>
2024-09-02 10:03:13 +0100

    create contracts

---
+++ contracts/Error.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+contract AirdropError {
+    error NotOwner();
+    error AlreadyClaimed();
+    error ClaimingEnded();
+    error InvalidProof();
+}
